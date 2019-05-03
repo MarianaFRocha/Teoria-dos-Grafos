089d459f69c86b1e01cc7791499c3145da138d22
Mariana Ferreira Rocha <marianaferrocha@gmail.com>
2019-05-03 18:11:40 -0300

    PP

---
+++ Percurso Profundidade/percusoProfundidade.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "grafo_lista.h"
+
+
+int alc, exp; 
+
+void PercusoRecursivo(int v, int pai, int *Alc, int *Exp){
+
+    int w;
+
+    Alc[v]=alc++;
+    
+
+
+    
+
+}
+
+
+void percursoProfundidadeRec(Grafo *g, int *Alc, int *Exp){
+        
+    
+
+    return ;
+}
